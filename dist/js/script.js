66dd01ef7520781d5c558c03355d7743781ce0e7
Kriva <edronbaton@proton.me>
2024-06-26 19:31:12 +0200

    section trending collections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,13 @@
-window.addEventListener("DOMContentLoaded", function () {
-    let hamburger = this.document.querySelector(".hamburger"),
-        menu = this.document.querySelector(".menu"),
-        close = this.document.querySelector(".menu__close");
+// window.addEventListener("DOMContentLoaded", function () {
+//     let hamburger = this.document.querySelector(".hamburger"),
+//         menu = this.document.querySelector(".menu"),
+//         close = this.document.querySelector(".menu__close");
 
-    hamburger.addEventListener("click", function () {
-        menu.classList.toggle("active")
-    })
+//     hamburger.addEventListener("click", function () {
+//         menu.classList.toggle("active")
+//     })
 
-    close.addEventListener("click", function () {
-        menu.classList.toggle("active")
-    })
-})
+//     close.addEventListener("click", function () {
+//         menu.classList.toggle("active")
+//     })
+// })
